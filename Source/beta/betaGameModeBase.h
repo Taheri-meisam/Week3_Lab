a8bcff0beeaa85c5d7014af0debe2ea7ba1d141c
Meisam Taheri <60437951+Taheri-meisam@users.noreply.github.com>
2023-01-19 16:54:53 +0100

    Source Files

---
+++ Source/beta/betaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "betaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BETA_API AbetaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
